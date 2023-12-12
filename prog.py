9cac23ae2b2f1a022aa6a2a9e79aec5717b759bc
yash-git-20 <yashbhavsar090@gmail.com>
2023-12-12 12:13:21 +0530

    added sub method

--- prog.py
+++ prog.py
@@ -1,8 +1,13 @@
 def add(a, b):
 	return a + b
 
-def main():
-	print(add(1,2))
+def sub(a, b):
+	return a - b
 
+def main():
+	print("addition of two numbers: " + str(add(1,2)))
+	print("substraction of two numbers: " + str(sub(10,5)))
+	
 if __name__ == '__main__':
 	main()
+
