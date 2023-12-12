79407dccb9484b7d20de0879832424973b9f395f
yash-git-20 <yashbhavsar090@gmail.com>
2023-12-12 12:26:26 +0530

    Fixed the requested changes

--- prog.py
+++ prog.py
@@ -1,12 +1,19 @@
+# Global Variables
+
+a = 1
+b = 2
+
+# This method is used to add two number
 def add(a, b):
 	return a + b
 
+# This method is used to subtract two number
 def sub(a, b):
 	return a - b
 
 def main():
-	print("addition of two numbers: " + str(add(1,2)))
-	print("substraction of two numbers: " + str(sub(10,5)))
+	print("addition of two numbers: " + str(add(a, b)))
+	print("substraction of two numbers: " + str(sub(a, b)))
 	
 if __name__ == '__main__':
 	main()
